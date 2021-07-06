07ebf905b19868ffab8be74d18fb4bad6628127e
Felipe Santos <felipesantosswengineer@gmail.com>
2021-07-05 21:17:01 -0300

    finished

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "kthl8822",
+  password: "postgres",
   host: "localhost",
   port: 5432,
-  database: "perntodo"
+  database: "postgres"
 });
 
 module.exports = pool;
